a2c50935766c87cc9e97df86224db24406f5dc5b
jackylam1985 <jackylam1985@gmail.com>
2017-07-09 22:32:29 +0800

    now can save stores to mongodb, added flash messages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,6 +10,8 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store'});
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
 }
