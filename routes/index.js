96c4c4df1634534a9ed54ed85b2c84d5c15e3046
jackylam1985 <jackylam1985@gmail.com>
2017-08-03 23:24:45 +0800

    added register page

--- routes/index.js
+++ routes/index.js
@@ -28,5 +28,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
