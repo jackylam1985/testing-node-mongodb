4126c817603f27fab33eb91821e284e1631863d4
jackylam1985 <jackylam1985@gmail.com>
2017-08-04 23:17:54 +0800

    added login function

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn,storeController.addStore);
 router.post('/add', 
     storeController.upload,
     catchErrors(storeController.resize), 
@@ -29,7 +28,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
-router.post('/login', userController.loginForm);
+router.post('/login', authController.login);
 
 router.get('/register', userController.registerForm);
 router.post('/register', 
@@ -37,5 +36,7 @@ router.post('/register',
     catchErrors(userController.register),
     authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
